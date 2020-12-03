364f97f5aae9f4bbfd0f031271378e959c6d8394
Garuda86 <garuda86@mail.ru>
2020-12-03 15:50:24 +0300

    HW6

--- src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -1,9 +1,12 @@
 package ru.javawebinar.topjava.service;
 
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.DataAccessException;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
@@ -21,6 +24,13 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest {
 
     @Autowired
     protected MealService service;
+
+    @Autowired
+    public Environment environment;
+
+    public boolean isJpaProfile() {
+        return environment.acceptsProfiles(org.springframework.core.env.Profiles.of(Profiles.JPA, Profiles.DATAJPA));
+    }
 
     @Test
     public void delete() {
@@ -104,6 +114,7 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest {
 
     @Test
     public void createWithException(){
+        Assume.assumeTrue("Validation not supported (JPA only)", isJpaProfile());
         validateRootCause(() -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "  ", 300), USER_ID), ConstraintViolationException.class);
         validateRootCause(() -> service.create(new Meal(null, null, "Description", 300), USER_ID), ConstraintViolationException.class);
         validateRootCause(() -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Description", 9), USER_ID), ConstraintViolationException.class);
